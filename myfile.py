41da3a65051710eb696b6dbb45d67a789b720b2c
Kathryn Salts <kathrynsalts@gmail.com>
2019-03-08 10:25:10 -0600

    welcome

--- myfile.py
+++ myfile.py
@@ -1,5 +1,16 @@
 def welcome():
+    print('******* Welcome to your checkbook *******')
+    
 def display_menu():
+    print(''' 
+    What would you like to do?
+
+    1) view current balance
+    2) record a debit (withdraw)
+    3) record a credit (deposit)
+    4) exit
+    ''')
+
 def view_balance():
 
 welcome()
