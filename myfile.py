fc43ab6c98a81ac3b44ef4259a72490f6b636c54
Kathryn Salts <kathrynsalts@gmail.com>
2019-03-08 12:08:04 -0600

    ordering

--- myfile.py
+++ myfile.py
@@ -19,58 +19,34 @@ def view_balance():
         balance = f.read().split('\n')
         for line in balance:
             if line.startswith('+'):
-                line.strip([+])
+                line.strip('+')
         new_bal = [int(line) for line in balance]
 
         print(sum(balance))
 
 view_balance()
 
-
-
-
-
 # def withdraw(money):
 #     pass
 # def deposit():
 #     pass
 
-# welcome()
-# display_menu()
-# while True:
-#     if user_input == '1':
-#         view_balance()
-#     elif user_input == '2':
-#         withdraw()
-#     elif user_input == '3':
-#         deposit()
-#     elif user_input == '4':
-#         sys.exit()
-#     else:
-#         print()
-#         print('ERROR. Invalid input, enter 1-4 only.')
-#         print()
-#     display_menu()
-    1) view current balance
-    2) record a debit (withdraw)
-    3) record a credit (deposit)
-    4) exit
-    ''')
-    user_input = input('Enter choice: ')
-    
-    if user_input == '1':
-        view_balance()
-    elif user_input =='2':
-        withdraw()
-    elif user_input == '3':
-        deposit()
-    elif user_input == '4':
-        exit_program()
-    else:
-        print()
-        print('ERROR. Invalid input; try again.')
-        print()
-        display_menu()
+
+user_input = input('Enter choice: ')
+
+if user_input == '1':
+    view_balance()
+elif user_input =='2':
+    withdraw()
+elif user_input == '3':
+    deposit()
+elif user_input == '4':
+    exit_program()
+else:
+    print()
+    print('ERROR. Invalid input; try again.')
+    print()
+    display_menu()
 
 
 def view_balance():
